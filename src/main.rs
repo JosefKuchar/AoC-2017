6c5b1f16c9621561db702c924b839a9dc46c3922
Josef Kuchař <email@josefkuchar.cz>
2017-12-01 15:02:44 +0100

    Day 1 part 1 shitty solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("input.txt").expect("not found");
+    let mut text = String::new();
+    let mut sum: usize = 0;
+    file.read_to_string(&mut text).expect("error");
+    
+    for (index, character) in text.chars().enumerate() {
+        if character == text.chars().nth((index + 1) % text.len()).unwrap() {
+            sum += character.to_digit(10).unwrap() as usize;
+        }
+    }
+
+    println!("{}", sum);
 }
