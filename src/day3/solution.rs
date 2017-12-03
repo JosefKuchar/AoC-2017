a5569978f437c87ca6dafc35b4697a83391ec628
Josef Kuchař <email@josefkuchar.cz>
2017-12-03 15:42:37 +0100

    Day 3

--- src/day3/solution.rs
+++ src/day3/solution.rs
@@ -23,12 +23,61 @@ fn get_size(input: usize) -> usize {
 }
 
 fn part1(input: usize) -> usize {
-    get_coordinates(265149);
-    unimplemented!();
+    get_coordinates(input);
+    //unimplemented!();
+    return 0;
+}
+
+fn part2() {
+    static DIRECTIONS: [[isize; 2]; 4] = [[1, 0], [0, -1], [-1, 0], [0, 1]];
+    let mut grid = [[0usize; 200]; 200];
+    let mut size = 1;
+    let mut counter = 0;
+    let mut steps = 0;
+    let mut x = 100;
+    let mut y = 100;
+    grid[100][100] = 1;
+    let mut direction = 0;
+
+    loop {
+        x = (x as isize + DIRECTIONS[direction][0]) as usize;
+        y = (y as isize + DIRECTIONS[direction][1]) as usize;
+        steps += 1;
+
+        for i in 0..3 {
+            for j in 0..3 {
+                if i == 1 && j == 1 {
+                    continue;
+                }
+                grid[x][y] += grid[x + i - 1][y + j - 1];
+            }
+        }
+
+        if steps == size {
+            direction += 1;
+            direction %= 4;
+            steps = 0;
+            counter += 1;
+
+            if counter == 2 {
+                counter = 0;
+                size += 1;
+            }
+        }
+
+        if grid[x][y] >= 265149 {
+            println!("{}", grid[x][y]);
+            break;
+        }
+
+        //println!("{}", grid[x][y]);
+    }
+
 }
 
 pub fn solve() {
     let solution1 = part1(265149);
+    part2();
     println!("{}", solution1);
 }
 
