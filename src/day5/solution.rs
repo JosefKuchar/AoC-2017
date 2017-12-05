38b5c595d5ad1f3ee1f6c86b4794bb8998e45292
Josef Kuchař <email@josefkuchar.cz>
2017-12-05 17:40:32 +0100

    Day 5

--- src/day5/solution.rs
+++ src/day5/solution.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
 
-
 fn load_input() -> Result<String, Box<Error>> {
     let mut file = File::open("src/day5/input.txt")?;
     let mut contents = String::new();
@@ -14,7 +13,8 @@ fn load_input() -> Result<String, Box<Error>> {
 pub fn solve() {
     let contents = load_input().unwrap();
     let solution1 = part1(&mut contents.split('\n').map(|x| x.parse().unwrap()).collect());
-    println!("{}", solution1);
+    let solution2 = part2(&mut contents.split('\n').map(|x| x.parse().unwrap()).collect());
+    println!("{} {}", solution1, solution2);
 }
 
 fn part1(instructions: &mut Vec<isize>) -> usize {
@@ -37,10 +37,40 @@ fn part1(instructions: &mut Vec<isize>) -> usize {
     return steps;
 }
 
+fn part2(instructions: &mut Vec<isize>) -> usize {
+    let mut steps = 0;
+    let mut index: isize = 0;
+    let mut prev_index = 0;
+
+    loop {
+        steps += 1;
+        index += instructions[index as usize];
+
+        if index < 0 || index as usize >= instructions.len() {
+            break;
+        }
+
+        if instructions[prev_index] >= 3 {
+            instructions[prev_index] -= 1;
+        } else {
+            instructions[prev_index] += 1;
+        }
+        
+        prev_index = index as usize;
+    }
+
+    return steps;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn part1() {
         assert_eq!(5, super::part1(&mut vec![0, 3, 0, 1, -3]));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(10, super::part2(&mut vec![0, 3, 0, 1, -3]));
     }
 }
