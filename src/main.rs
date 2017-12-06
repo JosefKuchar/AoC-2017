b89c2594fc8796d8451394d7c18e4551028b37e9
Josef Kuchař <email@josefkuchar.cz>
2017-12-06 16:44:01 +0100

    Display time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate time;
+
+use time::PreciseTime;
+
 mod day1;
 mod day2;
 mod day3;
@@ -6,10 +10,13 @@ mod day5;
 mod day6;
 
 fn main() {
+    let start = PreciseTime::now();
     day1::solve();
     day2::solve();
     day3::solve();
     day4::solve();
     day5::solve();
     day6::solve();
+    let end = PreciseTime::now();
+    println!("\nTime: {} ms", start.to(end).num_milliseconds());
 }
