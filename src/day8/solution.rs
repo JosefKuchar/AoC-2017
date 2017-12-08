75cc5c4e3b8ddb6ec9d8b0212c8cc7042ae4117b
Josef Kuchař <email@josefkuchar.cz>
2017-12-08 13:25:51 +0100

    Day 8

---
+++ src/day8/solution.rs
@@ -0,0 +1,70 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use std::collections::HashMap;
+
+fn load_input() -> Result<String, Box<Error>> {
+    let mut file = File::open("src/day8/input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    return Ok(contents);
+}
+
+pub fn solve() {
+    let input = load_input().unwrap();
+    let (part1, part2) = process(&input);
+    println!("{} {}", part1, part2);
+}
+
+fn process(input: &str) -> (isize, isize) {
+    let mut variables: HashMap<&str, isize> = HashMap::new();
+    let mut max_value = 0;
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        variables.entry(parts[0]).or_insert(0);
+        variables.entry(parts[4]).or_insert(0);
+        
+        let register_y = variables.get(parts[4]).unwrap().clone();
+        let register_x = variables.get_mut(parts[0]).unwrap();
+        let value_x: isize = parts[2].parse().unwrap();
+        let value_y: isize = parts[6].parse().unwrap();
+
+        let result = match parts[5] {
+            ">" => register_y > value_y,
+            "<" => register_y < value_y,
+            ">=" => register_y >= value_y,
+            "<=" => register_y <= value_y,
+            "!=" => register_y != value_y,
+            "==" => register_y == value_y,
+            _ => unimplemented!()
+        };
+
+        if !result {
+            continue;
+        } 
+        
+        if parts[1] == "inc" {
+            *register_x += value_x;
+        } else {
+            *register_x -= value_x;
+        }
+
+        if *register_x > max_value {
+            max_value = *register_x;
+        }
+    }
+
+    return (*variables.values().max().unwrap(), max_value);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn solve() {
+        assert_eq!((1, 10), super::process("b inc 5 if a > 1\n\
+                                            a inc 1 if b < 5 (57)\n\
+                                            c dec -10 if a >= 1\n\
+                                            c inc -20 if c == 10"));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod day4;
 mod day5;
 mod day6;
 mod day7;
+mod day8;
 
 fn main() {
     let start = PreciseTime::now();
@@ -19,6 +20,7 @@ fn main() {
     day5::solve();
     day6::solve();
     day7::solve();
+    day8::solve();
     let end = PreciseTime::now();
     println!("\nTime: {} ms", start.to(end).num_milliseconds());
 }
