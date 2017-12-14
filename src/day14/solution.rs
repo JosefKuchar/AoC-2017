9b18047af66b413dfbfcf90395aa6088a734f872
Josef Kuchař <email@josefkuchar.cz>
2017-12-14 18:26:33 +0100

    Day 14

--- src/day14/solution.rs
+++ src/day14/solution.rs
@@ -2,7 +2,8 @@ use std::usize;
 
 pub fn solve() {
     let part1 = part1("ffayrhll");
-    println!("{}", part1);
+    let part2 = part2("ffayrhll");
+    println!("{} {}", part1, part2);
 }
 
 fn part1(input: &str) -> usize {
@@ -13,18 +14,37 @@ fn part1(input: &str) -> usize {
     return sum;
 }
 
-fn part2(input: &str) {
-    let mut bool_grid: Vec<Vec<bool>> = Vec::new();
-    for index in 0..127 {
-        bool_grid.push(knot_hash(&format!("{}-{}", input, index)));
+fn part2(input: &str) -> usize {
+    let mut grid: Vec<Vec<bool>> = Vec::new();
+    for index in 0..128 {
+        grid.push(knot_hash(&format!("{}-{}", input, index)));
     }
 
     let mut group = 0;
-    for i in bool_grid {
-        for j in i {
+    for x in 0..128 {
+        for y in 0..128 {
+            if grid[x][y] {
+                let mut queue: Vec<Point> = Vec::new();
+                queue.push(Point::new(x as isize, y as isize));
+                let directions = [Point::new(1, 0), Point::new(-1, 0), Point::new(0, 1), Point::new(0, -1)];
 
+                while !queue.is_empty() {
+                    let element = queue.remove(0);
+                    grid[element.x as usize][element.y as usize] = false;
+
+                    for direction in &directions {
+                        let mut new_element: Point = element.clone();
+                        new_element.add(direction);
+                        if new_element.in_bounds() && grid[new_element.x as usize][new_element.y as usize] {
+                            queue.push(new_element);
+                        }
+                    }
+                }
+                group += 1;
+            }
         }
     }
+    return group;
 }
 
 fn knot_hash(input: &str) -> Vec<bool> {
@@ -59,10 +79,39 @@ fn knot_hash(input: &str) -> Vec<bool> {
     return binary.chars().map(|x| x == '1').collect();
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: isize,
+    y: isize
+}
+
+impl Point {
+    fn new(x: isize, y: isize) -> Point {
+        return Point {
+            x: x,
+            y: y
+        }
+    }
+
+    fn in_bounds(&self) -> bool {
+        return self.x >= 0 && self.x < 128 && self.y >= 0 && self.y < 128;
+    }
+
+    fn add(&mut self, point: &Point) {
+        self.x += point.x;
+        self.y += point.y;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn part1() {
         assert_eq!(8108, super::part1("flqrgnkx"));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(1242, super::part2("flqrgnkx"));
     }
 }
