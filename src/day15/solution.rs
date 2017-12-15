b55e2c7f12656b8d6addd49d48385b0ac750b6b3
Josef Kuchař <email@josefkuchar.cz>
2017-12-15 13:25:16 +0100

    Day 15

--- src/day15/solution.rs
+++ src/day15/solution.rs
@@ -1,6 +1,7 @@
 pub fn solve() {
     let part1 = part1(699, 124);
-    println!("{}", part1);
+    let part2 = part2(699, 124);
+    println!("{} {}", part1, part2);
 }
 
 fn part1(a: usize, b: usize) -> usize {
@@ -10,10 +11,6 @@ fn part1(a: usize, b: usize) -> usize {
     for _ in 0..40_000_000 {
         a = (a * 16807) % 2147483647;
         b = (b * 48271) % 2147483647;
-        /*
-        if &format!("{:032b}", a)[16..] == &format!("{:032b}", b)[16..] {
-            matches += 1;
-        }*/
         if a & 0xffff == b & 0xffff {
             matches += 1;
         }
@@ -25,10 +22,27 @@ fn part1(a: usize, b: usize) -> usize {
 fn part2(a: usize, b: usize) -> usize {
     let mut a = a;
     let mut b = b;
+    let mut matches = 0;
     for _ in 0..5_000_000 {
+        loop {
+            a = (a * 16807) % 2147483647;
+            if a % 4 == 0 {
+                break;
+            }
+        }
 
+        loop {
+            b = (b * 48271) % 2147483647;
+            if b % 8 == 0 {
+                break;
+            }
+        }
+
+        if a & 0xffff == b & 0xffff {
+            matches += 1;
+        }
     }
-    return 0;
+    return matches;
 }
 
 #[cfg(test)]
@@ -36,5 +50,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(588, super::part1(65, 8921));
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(309, super::part2(65, 8921));
     }
 }
